adf18bb79d6a14ae9a9a4e6f2bf71bf683c3f092
httpsyash <yashk19000@gmail.com>
2025-02-15 03:59:40 +0530

    about us 15 feb y

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,30 +2,15 @@
 import "./page.css"
 import CarousaL from "../components/Carousal.jsx"
 import Cards from "../components/Cards.jsx"
-
-
-export default function Home(){
+import AboutUs from "./AboutUs/Aboutus";
+import Pt from "./try";
+export default function Home() {
 	return (
 		<div>
-			<div>
-				<CarousaL/>
-			</div>
-			<div className = "Home_Page_Background">
-				<div className = "Home_Page_Content"> 
-					<div className="row">
-						<h1>What We Offer</h1>
-					</div>
-					<div className = "row">
-						<div className = "col-6">
-						<Cards/>
-						</div>
-						<div className = "col-6">
-						<Cards/>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Pt />
+			<AboutUs />
+
 		</div>
 
-		);
+	);
 }
