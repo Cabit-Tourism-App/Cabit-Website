d8ae78b829343f99728fc18e55fc19f5fa0f9bc8
= <=>
2025-02-28 03:00:53 +0530

    modified home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,10 +27,10 @@ export default function Home() {
 							<br />
 							<div className="row">
 								<div className="Flex_Row">
-									<ImageCards width="50vh" height="70vh" heading = "Tour Planner" />
-									<ImageCards width="50vh" height="70vh" heading = "Cab Booking"  />
-									<ImageCards width="50vh" height="70vh" heading = "Best Destinations "  />
-									<ImageCards width="50vh" height="70vh" heading = "No Lang-Barriers"  />
+									<ImageCards width="50vh" height="70vh" heading = "Tour Planner" text = "Customize your trip with a personalized itinerary." source = "/images/homepage/col1.jpeg"/>
+									<ImageCards width="50vh" height="70vh" heading = "Cab Booking"  text = "Book cabs with drivers who understand your language for a smooth journey." source = "/images/homepage/col2.jpeg"/>
+									<ImageCards width="50vh" height="70vh" heading = "Best Destinations "  text = "Discover top travel spots in India, handpicked for every type of traveler." source = "/images/homepage/col3.jpeg"/>
+									<ImageCards width="50vh" height="70vh" heading = "No Lang-Barriers"  text = "Overcome language challenges with in-app translation tools." source = "/images/homepage/col4.jpeg"/>
 								</div>
 							</div>
 							<br />
@@ -45,7 +45,7 @@ export default function Home() {
 									<br />
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point1.jpeg" style = {{width:"100%" , borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Choose Destination</h3>
 											<p>Select your desired travel destination and customize your itinerary.</p>
@@ -54,7 +54,7 @@ export default function Home() {
 									<br />
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point2.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Make Payment</h3>
 											<p>Securely complete your payment for the trip and cab services.</p>
@@ -62,7 +62,7 @@ export default function Home() {
 									</div>
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point3.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Reach Airport on selected day</h3>
 											<p>Arrive at the airport on your selected date and start your journey!</p>
@@ -71,7 +71,7 @@ export default function Home() {
 								</div>
 								<div className="col-5">
 										{/*An introductory video...*/}
-									<ImageBox/>
+									<ImageBox source = "/images/homepage/couples.jpeg"/>
 								</div>
 								<div className="col-1"></div>
 
@@ -88,26 +88,26 @@ export default function Home() {
 								<div className="col-1"></div>
 								<div className="col-5">
 										{/*An introductory video...*/}
-									<ImageBox/>
+									<ImageBox source = "/images/homepage/translator.jpeg"/>
 								</div>
 								<div className = "col-5" style={{ textAlign: "left" }}>
-									<div className="row">
+
 										<div className = "Flex_Row">
 											<h1> How we Make your travel better</h1>
 											<br/>
 										</div>
-									</div>
+
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point4.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Plan ahead Plan better</h3>
 											<p>Effortlessly plan your trip with customized itineraries, seamless ride integration, and real-time updates for a stress-free journey.</p>
 										</div>
 									</div>
-							
+
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point5.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Multilingual Support</h3>
 											<p>Plan your trip effortlessly with our multilingual drivers and built-in translator, ensuring clear communication and a seamless journey.</p>
@@ -115,7 +115,7 @@ export default function Home() {
 									</div>
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point6.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Affordable Prices/Cashless Payments</h3>
 											<p>Enjoy affordable pricing with no hidden charges and seamless cashless payments for a hassle-free travel experience</p>
@@ -136,7 +136,7 @@ export default function Home() {
 									<br />
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point7.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Verified & Trained Drivers</h3>
 											<p>SAll drivers undergo background checks and professional training to ensure a safe and reliable travel experience for everyone.</p>
@@ -145,7 +145,7 @@ export default function Home() {
 									<br />
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point8.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Live Tracking & 24/7 Assistance</h3>
 											<p>Real-time GPS tracking and a dedicated support team provide constant monitoring and immediate help whenever needed</p>
@@ -153,7 +153,7 @@ export default function Home() {
 									</div>
 
 									<div className="Flex_Row">
-										<div className="col-1"><img src = "images/download.jpeg" style = {{width:"100%"}}/></div>
+										<div className="col-1"><img src = "/images/homepage/point9.jpeg" style = {{width:"100%", borderRadius:"100px"}}/></div>
 										<div className="col-10">
 											<h3>Secure & Comfortable Rides for All</h3>
 											<p>Features like ride detail sharing, emergency alerts, and a safe travel environment ensure a worry-free journey for every passenger.</p>
@@ -162,7 +162,7 @@ export default function Home() {
 								</div>
 								<div className="col-5">
 										{/*An introductory video...*/}
-									<ImageBox/>
+									<ImageBox source = "/images/homepage/goodman.jpeg"/>
 								</div>
 								<div className="col-1"></div>
 
@@ -181,11 +181,11 @@ export default function Home() {
 								  <div className="d-flex justify-content-center" style={{ gap: "20px" }}>
 
 								    <div className="text-center">
-								      <ImageBox width="40vh" height="40vh"/>
+								      <ImageBox width="40vh" height="40vh" source= "/images/homepage/1.png" />
 								      <p className="mt-2 fw-semibold">Cabit User: Bike-Taxi, Auto & Cabs</p>
 								    </div>
 								    <div className="text-center">
-								      <ImageBox width="40vh" height="40vh"/>
+								      <ImageBox width="40vh" height="40vh"source= "/images/homepage/2.png" />
 								      <p className="mt-2 fw-semibold">Cabit Driver: Drive & Earn</p>
 								    </div>
 
