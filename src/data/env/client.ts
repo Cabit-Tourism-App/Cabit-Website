a2c098fbbbace2ac86e303c99c3207ec07e5e88f
= <=>
2025-03-23 13:49:49 +0530

    a

--- src/data/env/client.ts
+++ src/data/env/client.ts
@@ -1,7 +1,8 @@
-import { createEnv } from "@t3-oss/env-nextjs"
+/*import { createEnv } from "@t3-oss/env-nextjs"
 
 export const env = createEnv({
   client: {},
   experimental__runtimeEnv: {},
   emptyStringAsUndefined: true,
 })
+*/
